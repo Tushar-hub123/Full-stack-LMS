87c46fe05dc1782a17ac22965cf19bff3607f118
L_K_B <lalitbansal8322@gmail.com>
2026-03-23 13:20:28 +0530

    update url

--- backend/server.js
+++ backend/server.js
@@ -20,13 +20,16 @@ const __dirname  = path.dirname(__filename);
 const app = express();
 
 /* ── Middleware ─────────────────────────────────────── */
+// app.use(cors({
+//   origin: [
+//     "https://lms-frontend-eta-orcin.vercel.app"
+//   ],
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: [
-    "https://lms-frontend-eta-orcin.vercel.app"
-  ],
-  credentials: true,
+  origin: true,
+  credentials: true
 }));
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
